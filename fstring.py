bd5ab0f7552159387343239a28d72dfdcc5054ec
johnson vj <121477725+samplejo@users.noreply.github.com>
2023-01-04 18:56:37 +0530

    Add files via upload

---
+++ fstring.py
@@ -0,0 +1,4 @@
+#Take name and age of the person from input and display on the console using fstring
+name="johnson valloor jose"
+age=24
+print(f"Hello,My name is {name} and I am {age} years old") 
