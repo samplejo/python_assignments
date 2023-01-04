d7f5963141a43f34f4f44e196f8de4d98219f43a
johnson vj <121477725+samplejo@users.noreply.github.com>
2023-01-04 18:57:31 +0530

    Add files via upload

---
+++ ifsb.py
@@ -0,0 +1,13 @@
+#Take whole number,floating point number,string boolean data.check each of those datatype
+# Whole number or integer
+j=30
+print(type(j))
+#floating point number
+o=7.19
+print(type(o))
+#string
+h="Good boy"
+print(type(h))
+#boolean
+n=True
+print(type(n))
